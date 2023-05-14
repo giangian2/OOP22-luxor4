495f132049cd18fdd05f64b9cf79689d28aede73
Gianluca Bianchi <gianluca.bianchi8@studio.unibo.it>
2023-05-14 14:18:33 +0200

    path builder

---
+++ src/test/java/it/unibo/utils/PathTest.java
@@ -0,0 +1,31 @@
+package it.unibo.utils;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+public class PathTest {
+
+    @Test
+    void testPathBuilder() {
+        try {
+            var test = new Path.PathBuilder();
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
