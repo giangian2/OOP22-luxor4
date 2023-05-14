d0a58aa19bd76bbe4a0799b85057055365f633b3
Fede <federica.guiducci3@studio.unibo.it>
2023-05-14 13:21:53 +0200

    aggiornamento1

--- src/main/java/it/unibo/model/GameObject.java
+++ src/main/java/it/unibo/model/GameObject.java
@@ -1,5 +1,69 @@
 package it.unibo.model;
 
-public class GameObject {
+import main.java.it.unibo.common.*;
+import main.java.it.unibo.input.*;
+import main.java.it.unibo.graphics.*;
+ 
+
+public class GameObjects{
+
+    private P2d pos; 
+    private string color;
+    private V2d vel; 
+    private InputComponent input;
+    private GraphicsComponent graph; 
+
+    protected GameObjects(P2d pos, string color, V2d vel, InputComponent input, GraphicsComponent graph){
+        this.posball= posball;
+        this.color= color;
+        this.vel= vel;
+        this.input = input;
+        this.graph = graph;
+    }
+
+    public void setPos(P2d pos){
+		this.pos = pos;
+	}
+
+    public void setColor(string color){
+        this.color= color;
+    }
+
+    public void setVel(V2d vel){
+		this.vel = vel;
+	}
+
+    public P2d getCurrentPos(){
+		return pos;
+	}
+	
+	public V2d getCurrentVel(){
+		return vel;
+	}
+
+    public void updateInput(InputController c){
+		input.update(this, c);
+	}
+
+    public void updateGraphics(Graphics g){
+		graph.update(this, g);
+	}
+
 
 }
+
+
+
+	
+	
+	
+	
+
+	
+
+	
+	
+	
+	
+
+
