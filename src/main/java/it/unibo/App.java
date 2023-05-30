baa61c8d0c16fcea2bf0aa484a1e7d5128aa8f0b
chiaradenardi <chiara.denardi2@studio.unibo.it>
2023-05-30 17:56:27 +0200

    class menu

--- src/main/java/it/unibo/App.java
+++ src/main/java/it/unibo/App.java
@@ -3,10 +3,15 @@ package it.unibo;
 import it.unibo.core.impl.GameEngineImpl;
 import it.unibo.core.api.GameEngine;
 
+import it.unibo.graphics.impl.MenuGame;
+
 public class App {
     public static void main(String[] args) {
         GameEngine engine = new GameEngineImpl();
         engine.initGame();
         engine.mainLoop();
+        
+        MenuGame menuGame = new MenuGame();
+        menuGame.setVisible(true);
     }
 }
