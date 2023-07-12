8beebb001fc313ca864f7c244deaea5d97c364e1
chiaradenardi <chiara.denardi2@studio.unibo.it>
2023-07-12 15:28:17 +0200

    errori prova

--- src/main/java/it/unibo/physics/impl/CannonPhysicsComponent.java
+++ src/main/java/it/unibo/physics/impl/CannonPhysicsComponent.java
@@ -7,37 +7,24 @@ import it.unibo.model.World;
 import it.unibo.utils.P2d;
 import it.unibo.utils.V2d;
 
-<<<<<<< HEAD
 public class CannonPhysicsComponent extends PhysicsComponent {
 
     private double cannonPosX;
     private double cannonPosY;
     private double cannonVelX;
-=======
-/*public class CannonPhysicsComponent extends PhysicsComponent{
->>>>>>> 19d01f0726f2e72c4be1c725070f53c32a28c110
 
     @Override
     public void update(long dt, GameObject obj, World w) {
-
         super.update(dt, obj, w);
 
         if (obj instanceof Cannon) {
             Cannon cannon = (Cannon) obj;
-            P2d cannonPos = cannon.getCurrentPos();  // Ottieni la posizione corrente del cannone
-            cannonPosX = cannonPos.x;  // Aggiungi la velocità x corrente del cannone
-            V2d cannonVel = cannon.getCurrentVel();  // Ottieni la velocità corrente del cannone
-            cannonVelX = cannonVel.x; 
+            P2d cannonPos = cannon.getCurrentPos();
+            cannonPosX = cannonPos.x;
+            cannonPosY = cannonPos.y;
+
+            V2d cannonVel = cannon.getCurrentVel();
+            cannonVelX = cannonVel.x;
         }
     }
-}*/
-
-public class CannonPhysicsComponent extends PhysicsComponent {
-
-    @Override
-    public void update(long dt, GameObject obj, World w) {
-       
-
-    }
-
 }
