3cbc80d70e433bb1299e8173178a184336a42b75
Federica Guiducci - federica.guiducci3@studio.unibo.it <federica.guiducci3@studio.unibo.it>
2023-07-21 14:13:14 +0200

    edit frame

--- src/main/java/it/unibo/graphics/impl/CannonGraphicsComponent.java
+++ src/main/java/it/unibo/graphics/impl/CannonGraphicsComponent.java
@@ -6,8 +6,6 @@ public class CannonGraphicsComponent implements GraphicsComponent {
     @Override
     public void update(GameObject obj, Graphics c) {
        
-        //qui non so che scrivere (quello sotto non va bene)
-       // c.setColor(Color.RED);
-        //c.fillOval((int) obj.getPos().x, (int) obj.getPos().y, (int) obj.getRadius(), (int) obj.getRadius());
+      
     }
 }
