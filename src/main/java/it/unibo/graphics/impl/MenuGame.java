a88773135ed26c70b7578a20b56ef2f59969d1dc
Chiara De Nardi - chiara.denardi2@studio.unibo.it <chiara.denardi2@studio.unibo.it>
2023-07-26 10:26:40 +0200

    jframe menu

--- src/main/java/it/unibo/graphics/impl/MenuGame.java
+++ src/main/java/it/unibo/graphics/impl/MenuGame.java
@@ -4,26 +4,37 @@ import it.unibo.core.impl.GameEngineImpl;
 import it.unibo.enums.Levels;
 
 import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MenuGame extends JFrame {
 
     private Levels selectedLevel;
+    private JPanel mainPanel;
 
     /**
      * The initial menu of the game.
-     */
+     * */
     public MenuGame() {
 
         setTitle("Luxor");
@@ -31,15 +42,80 @@ public class MenuGame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        /**
-         * Principal panel.
-         */
-        JPanel mainPanel = new JPanel(new GridLayout(2, 1));
-        add(mainPanel);
 
         /**
-         * Label.
-         */
+        * Principal panel.
+        * */
+        mainPanel = new JPanel(new GridLayout(2, 1));
+        add(mainPanel);
+
+        showMainMenu();
+    }
+
+    private JButton createButton(String text, ActionListener actionListener) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.PLAIN, 16));
+        button.addActionListener(actionListener);
+        return button;
+    }
+        
+    private void showHelpMenu() {
+        mainPanel.removeAll();
+        mainPanel.revalidate();
+        mainPanel.repaint();
+
+        JPanel helpPanel = new JPanel();
+        helpPanel.setLayout(new BoxLayout(helpPanel, BoxLayout.Y_AXIS));
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.setFont(new Font("Arial", Font.PLAIN, 16));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        mainPanel.add(scrollPane, BorderLayout.CENTER); // Add the scrollPane to the mainPanel
+            
+        // Adding some vertical space between the textArea and the "Back" button
+        helpPanel.add(Box.createVerticalStrut(10));
+
+        // Load text from file
+        try {
+            String filePath = "src/main/resources/help/help.txt";
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            StringBuilder content = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            reader.close();
+            textArea.setText(content.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JButton back = new JButton("Back");
+        back.setFont(new Font("Arial", Font.PLAIN, 16));
+        back.setAlignmentX(Component.CENTER_ALIGNMENT);
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                showMainMenu();
+            }
+        });
+
+        helpPanel.add(Box.createVerticalGlue());
+        helpPanel.add(back);
+        mainPanel.add(helpPanel);
+    }
+
+    public void showMainMenu() {
+        mainPanel.removeAll();
+        mainPanel.revalidate();
+        mainPanel.repaint();
+
         JPanel labelPanel = new JPanel(new GridBagLayout());
         mainPanel.add(labelPanel);
 
@@ -51,75 +127,48 @@ public class MenuGame extends JFrame {
         constraints.anchor = GridBagConstraints.CENTER;
         labelPanel.add(label, constraints);
 
-        /**
-         * Button Help.
-         */
         JPanel buttonPanel = new JPanel();
         mainPanel.add(buttonPanel);
 
-        JButton help = new JButton("Help");
-        help.setFont(new Font("Arial", Font.PLAIN, 16));
-        help.addActionListener(new ActionListener() {
+        JButton help = createButton("Help", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                setVisible(false); // Nasconde la finestra del menu
-                MenuHelp MenuHelp = new MenuHelp(MenuGame.this); // Passa il riferimento alla finestra del menu alla
-                                                                 // finestra di help
-                MenuHelp.setVisible(true); // Mostra la finestra di help
+                showHelpMenu();
             }
         });
-
         buttonPanel.add(help);
 
-        /**
-         * Button start.
-         */
-        JButton startGame = new JButton("Start Game");
-        startGame.setFont(new Font("Arial", Font.PLAIN, 16));
-
-        startGame.addActionListener(new ActionListener() {
+        JButton startGame = createButton("Start Game", new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 final Thread thread = new Thread() {
                     @Override
                     public void run() {
-
                         new GameEngineImpl(selectedLevel).initGame();
                     }
                 };
                 thread.start();
-
                 System.out.println("Game started!");
                 dispose();
             }
         });
-
         buttonPanel.add(startGame);
 
-        // Button Livelli.
-        JButton levelsButton = new JButton("Livelli");
-        levelsButton.setFont(new Font("Arial", Font.PLAIN, 16));
-        levelsButton.addActionListener(new ActionListener() {
+        JButton levelsButton = createButton("Livelli", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-
                 Object[] options = Levels.values();
-
                 Levels selected = (Levels) JOptionPane.showInputDialog(
-                        MenuGame.this, // Frame genitore
-                        "Seleziona un livello", // Testo del messaggio
-                        "Selezione Livello", // Titolo del dialogo
-                        JOptionPane.PLAIN_MESSAGE, // Tipo di messaggio
-                        null, // Icona personalizzata (null per l'icona di default)
-                        options, // Opzioni da mostrare nell'elenco
-                        options[0] // Opzione preselezionata (in questo caso, la prima costante dell'enum)
+                    MenuGame.this, // Frame genitore
+                    "Seleziona un livello", // Testo del messaggio
+                    "Selezione Livello", // Titolo del dialogo
+                    JOptionPane.PLAIN_MESSAGE, // Tipo di messaggio
+                    null, // Icona personalizzata (null per l'icona di default)
+                    options, // Opzioni da mostrare nell'elenco
+                    options[0] // Opzione preselezionata (in questo caso, la prima costante dell'enum)
                 );
 
                 if (selected != null) {
                     selectedLevel = selected;
                     System.out.println("Livello selezionato: " + selectedLevel.getLevelName());
-                    // Ora puoi utilizzare la variabile selectedLevel per richiamare il livello
-                    // selezionato
-                    // e fare le azioni necessarie.
                 }
             }
         });
@@ -129,4 +178,7 @@ public class MenuGame extends JFrame {
         startGame.setAlignmentX(Component.CENTER_ALIGNMENT);
     }
 
+    public Levels getSelectedLevel() {
+        return selectedLevel;
+    }
 }
