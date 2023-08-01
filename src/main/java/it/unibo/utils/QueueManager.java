17346281471132d7f899e085f1a80b996914c2c7
Francesco Candoli <francesco.candoli@studio.unibo.it>
2023-08-01 10:43:54 +0200

    javadoc QueueManager

--- src/main/java/it/unibo/utils/QueueManager.java
+++ src/main/java/it/unibo/utils/QueueManager.java
@@ -9,12 +9,22 @@ import it.unibo.enums.BallColor;
 import it.unibo.enums.Direction;
 import it.unibo.model.Ball;
 
+/**
+ * The QueueManager manages a list of Ball objects associated with a Path.
+ */
 public class QueueManager {
 
     public Path path;
     public List<Ball> balls;
     private int steps;
 
+    /**
+     * Constructs a QueueManager with the specified parameters.
+     *
+     * @param nBalls The number of balls in the list
+     * @param steps The number of times the balls will be moved forward.
+     * @param xmlPathSrc The path of the xml file containing the Path.
+     */
     public QueueManager(int nBalls, int steps, String xmlPathSrc) {
         path = new Path.PathBuilder(xmlPathSrc).build();
         this.balls = new ArrayList<>();
@@ -23,13 +33,22 @@ public class QueueManager {
         this.steps = steps;
     }
 
+    /**
+     * Instatiate a list of n Ball with the following properties:
+     * <ul>
+     *     <li>The list does not contain more than two adjacent balls of the same color</li>
+     *     <li>The positions of the balls will follow the Path vertex</li>
+     * </ul>
+     * @param n The number of balls in the list.
+    */
     private void instatiate(int n) {
         var factory = GameObjectsFactory.getInstance();
         var pos = path.getFirst();
         Direction direction;
 
-        // crea una lista di palline che vada bene
-
+        /**
+         * Create the list of balls following the color property
+         */
         for (int i = 0; i < n; i++) {
             this.balls.add(factory.createBall(new P2d(0, 0), new V2d(0, 0), BallColor.getRandomColor()));
             if (i > 2) {
@@ -41,7 +60,9 @@ public class QueueManager {
             }
         }
 
-        // assegna le posizioni
+        /**
+         * Set the positions of the balls following the Path
+         */
         for (int i = 0; i < balls.size(); i++) {
             balls.get(i).setPos(pos);
             direction = path.getMove(pos);
@@ -51,10 +72,20 @@ public class QueueManager {
 
     }
 
+    /**
+     * 
+     * @param ball The ball of which you want to know the next movement
+     * @return The direction of the ball next movement
+     * @see Path
+     */
     public Direction getMove(Ball ball) {
         return path.getMove(ball.getCurrentPos());
     }
 
+    /**
+     * Move balls contained in the list by one pixel starting from a specified index.
+     * @param startIndex The index from which the shift must start.
+     */
     public void shiftBalls(int startIndex) {
         boolean firstShifting = true;
 
@@ -90,6 +121,8 @@ public class QueueManager {
                     case RIGHT:
                         this.balls.get(i).setPos(new P2d(currentPos.x + 1, currentPos.y));
                         break;
+                    default:
+                        break;
                 }
                 firstShifting = false;
             }
@@ -97,12 +130,19 @@ public class QueueManager {
 
     }
 
+    /**
+     * Call steps-time the method {@link #shiftBalls()}
+     */
     public void shiftBallsStepsTime() {
         for (int i = 0; i < steps; i++) {
             shiftBalls(0);
         }
     }
 
+    /**
+     * Search if there are sub-lists of at least three adjacent balls with the same color in the ball list
+     * @return The sum of the sub-lists founded
+     */
     public List<Ball> getCloseByThree() {
         var ballList = this.balls;
         List<Ball> returnList = new ArrayList<Ball>();
