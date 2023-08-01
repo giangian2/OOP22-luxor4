682ab6249818fab7b1dbe911dd995c1f629a86c3
Chiara De Nardi - chiara.denardi2@studio.unibo.it <chiara.denardi2@studio.unibo.it>
2023-08-01 12:12:52 +0200

    delete class cannonPhysicsComp

--- src/main/java/it/unibo/App.java
+++ src/main/java/it/unibo/App.java
@@ -2,12 +2,21 @@ package it.unibo;
 
 import it.unibo.graphics.impl.MenuGame;
 
+/**
+ * The main class for starting the application.
+ */
 public class App {
-  public static void main(String[] args) {
-    // GameEngine engine = new GameEngineImpl(null);
-    // engine.initGame();
-    // engine.mainLoop();
-    MenuGame mainFrame = new MenuGame();
-    mainFrame.setVisible(true);
-  }
+
+    /**
+     * The main entry point of the application.
+     *
+     * @param args The command-line arguments (not used in this application).
+     */
+    public static void main(String[] args) {
+      /**
+       * Create and show the main menu frame.
+       */
+        MenuGame mainFrame = new MenuGame();
+        mainFrame.setVisible(true);
+    }
 }
