05ab6225316c957e7af55e18789a9c83a437efc3
Francesco Candoli <francesco.candoli@studio.unibo.it>
2023-08-01 13:13:23 +0200

    javadoc Ball

--- src/main/java/it/unibo/model/Ball.java
+++ src/main/java/it/unibo/model/Ball.java
@@ -5,31 +5,66 @@ import it.unibo.graphics.impl.BallGraphicsComponent;
 import it.unibo.input.api.InputComponent;
 import it.unibo.model.api.BoundingBox;
 import it.unibo.physics.api.PhysicsComponent;
-import it.unibo.utils.*;
+import it.unibo.utils.P2d;
+import it.unibo.utils.V2d;
 
+/**
+ * Represents a ball object in the game.
+ */
 public class Ball extends GameObject {
 
     private BallColor color;
-    public static final int IMAGE_DIAMETER = 25;
-    public static final int RADIUS = 0;
 
-    public Ball(Type type, P2d pos, BallColor color, V2d vel, InputComponent input, BoundingBox bbox,
-            PhysicsComponent physics, BallGraphicsComponent graph) {
+    /**
+     * Diameter of the ball.
+     */
+    public static final int IMAGE_DIAMETER = 25;
+
+    /**
+     * Constructor for an istance of Ball.
+     * @param type Specified type of game object.
+     * @param pos Position (coordinates)
+     * @param color Color
+     * @param vel Velocity vector
+     * @param input Input component 
+     * @param bbox Bounding box
+     * @param physics Physics component
+     * @param graph Graphic component
+     */
+    public Ball(final Type type, final P2d pos, final BallColor color,
+                final V2d vel, final InputComponent input, final BoundingBox bbox,
+                final PhysicsComponent physics, final BallGraphicsComponent graph) {
 
         super(Type.BALL, pos, vel, input, bbox, graph, physics);
         this.color = color;
     }
 
-    public void setColor(BallColor color) {
+    /**
+     * Set the color of the ball.
+     * @param color Color to set
+     * @see BallColor
+     */
+    public void setColor(final BallColor color) {
         this.color = color;
     }
-
+    /**
+     * Get the color of the ball.
+     * @see BallColor
+     * @return The color of the ball
+     */
     public BallColor getColor() {
         return this.color;
     }
 
-    public boolean isNear(Ball ball) {
-        return Math.abs(this.getCurrentPos().sumOfAxis() - ball.getCurrentPos().sumOfAxis()) < (IMAGE_DIAMETER + 2);
+    /**
+     * Method that return if a ball(a) if the distance beetween a and this ball
+     * is less than its diameter + a little approximation).
+     * 
+     * @param a Ball to check if is near 
+     * @return True if the ball is near else false 
+     */
+    public boolean isNear(final Ball a) {
+        return Math.abs(this.getCurrentPos().sumOfAxis() - a.getCurrentPos().sumOfAxis()) < (IMAGE_DIAMETER + 2);
     }
 
 }
