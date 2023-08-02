e7e2f4ebdc37480592ba937982f9db2b886ac72a
Francesco Candoli <francesco.candoli@studio.unibo.it>
2023-08-02 09:30:20 +0200

    javadoc BallColor

--- src/main/java/it/unibo/enums/BallColor.java
+++ src/main/java/it/unibo/enums/BallColor.java
@@ -2,26 +2,59 @@ package it.unibo.enums;
 
 import java.awt.Color;
 import java.util.Random;
-
+/**
+ * Enum that represents all possible colors of a ball,
+ * it uses Color class.
+ * 
+ * @see java.awt.Color
+ */
 public enum BallColor {
+
+    /**
+     * Color red.
+     */
     RED(Color.RED),
+
+    /**
+     * Color blue.
+     */
     BLUE(Color.BLUE),
+
+    /**
+     * Color green.
+     */
     GREEN(Color.GREEN),
+
+    /**
+     * Color yellow.
+     */
     YELLOW(Color.YELLOW);
 
     private Color color;
 
-    private BallColor(Color color){
-        this.color=color;
+    /**
+     * Private constructor of BallColor.
+     * @param color Color representation
+     */
+    BallColor(final Color color) {
+        this.color = color;
     }
 
-    public Color getBallColor(){
+    /**
+     * Return the color of the enum element.
+     * @return Color represented by enum element.
+     */
+    public Color getBallColor() {
         return this.color;
     }
 
-    public static BallColor getRandomColor(){
+    /**
+     * Get a random element of the enum.
+     * @return Random ballColor.
+     */
+    public static BallColor getRandomColor() {
         BallColor[] colors = BallColor.values();
-        Random rand= new Random();
+        Random rand = new Random();
         return colors[rand.nextInt(colors.length)];
     }
 }
