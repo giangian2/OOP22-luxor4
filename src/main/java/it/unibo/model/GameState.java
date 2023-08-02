18d0aec4418561006a108e86bcc42b518d469f60
Francesco Candoli <francesco.candoli@studio.unibo.it>
2023-08-02 10:08:57 +0200

    javadoc GameState

--- src/main/java/it/unibo/model/GameState.java
+++ src/main/java/it/unibo/model/GameState.java
@@ -1,13 +1,16 @@
 package it.unibo.model;
 
-import it.unibo.utils.Path;
-import it.unibo.utils.QueueManager;
+
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.core.api.Level;
 import it.unibo.enums.Direction;
 import it.unibo.events.api.WorldEventListener;
 import it.unibo.input.impl.KeyboardInputController;
 
+/**
+ * Represents the state of the game, including the score, world, pause state, and level.
+ * This class manages the game state and provides methods for updating the state based on game events.
+ */
 @SuppressFBWarnings(value = { "EI_EXPOSE_REP" }, justification = "I prefer to suppress these FindBugs warnings")
 public class GameState {
 
@@ -16,7 +19,13 @@ public class GameState {
     private boolean pause;
     private Level level;
 
-    public GameState(WorldEventListener l, Level level) {
+    /**
+     * Constructs a new GameState object with the specified event listener and level.
+     *
+     * @param l The WorldEventListener to handle game events.
+     * @param level The Level object representing the current game level.
+     */
+    public GameState(final WorldEventListener l, final Level level) {
         score = 0;
         pause = false;
         this.level = level;
@@ -24,44 +33,70 @@ public class GameState {
         world.setEventListener(l);
     }
 
+    /**
+     * Retrieves the World object representing the game world.
+     *
+     * @return The World.
+     */
     public World getWorld() {
         return world;
     }
 
+    /**
+     * Increases the current score by 1.
+     */
     public void incScore() {
         score++;
     }
 
+    /**
+     * Decreases the current score by 1.
+     */
     public void decScore() {
         score--;
     }
 
+    /**
+     * Retrieves the current score of the game.
+     *
+     * @return The current score.
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Loads the level into the game world.
+     * This method initializes the game world based on the current level.
+     */
     private void loadLevel() {
         this.world = this.level.loadLevel();
     }
 
+    /**
+     * Checks if the game is over.
+     *
+     * @return true if the game is over, false otherwise.
+     */
     public boolean isGameOver() {
         var res = this.getWorld().moveSingleBall(this.getWorld().getQueue().get(this.getWorld().getQueue().size() - 1));
-        if (res == Direction.NONE) {
-            return true;
-        } else {
-            return false;
-        }
+        return res == Direction.NONE;
 
     }
 
+    /**
+     * Checks if the player has won the game.
+     *
+     * @return true if the player has won, false otherwise.
+     */
     public boolean isWin() {
-        if (this.getWorld().getQueue().size() == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.getWorld().getQueue().size() == 0;
     }
 
+    /**
+     * Toggles the pause state of the game.
+     * If the game is paused, it will be resumed, and vice versa.
+     */
     public void changePauseState() {
         if (pause) {
             getWorld().unpauseBackgroundSound();
@@ -72,7 +107,12 @@ public class GameState {
         }
     }
 
-    public void update(long dt) {
+    /**
+     * Updates the game state based on the elapsed time.
+     *
+     * @param dt The elapsed time since the last update.
+     */
+    public void update(final long dt) {
         if (!pause) {
             world.updateState(dt);
 
@@ -84,7 +124,13 @@ public class GameState {
         }
     }
 
-    public void processInput(KeyboardInputController controller) {
+    /**
+     * Processes the user input using the provided KeyboardInputController.
+     * This method updates the state of the game world based on the user's input.
+     *
+     * @param controller The KeyboardInputController to handle user input.
+     */
+    public void processInput(final KeyboardInputController controller) {
         if (!pause) {
             world.getCannon().updateInput(controller);
         }
